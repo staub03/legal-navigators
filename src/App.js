a259d5b822c295980fa3e0411802fd2d251ec7ae
Dozo <64401443+staub03@users.noreply.github.com>
2023-06-29 01:33:51 +0200

    Added about us site

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom"
 import LandingPage from "./pages/landingpage"
+import AboutPage from "./pages/aboutus"
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={ <LandingPage/> } />
+        <Route path="/about" element={ <AboutPage/> } />
       </Routes>
     </div>
   )
